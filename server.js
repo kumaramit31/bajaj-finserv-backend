295a45a8fab4864f114784714c348d228d687175
Amit Kumar <137507051+kumaramit31@users.noreply.github.com>
2025-02-21 18:58:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",  // Allow all domains
+    methods: ["GET", "POST"]
+  }));
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
